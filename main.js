30bec61c473bc53e3d593c23db44832148633afe
Cooper <kcoopermiller9@gmail.com>
2023-07-30 14:21:37 -0500

    transitions between scenes

---
+++ main.js
@@ -0,0 +1,59 @@
+import s1 from "./scene1.js";
+import s2 from "./scene2.js";
+import s3 from "./scene3.js";
+
+// Include your scenes as JavaScript functions
+const scenes = [s1, s2, s3];
+
+let currentSceneIndex = 0;
+
+// Function to generate the current scene
+function generateScene() {
+    // Clear the previous scene
+    d3.select("#my_dataviz").html("");
+
+    // Scene descriptions
+    const sceneDescriptions = [
+        "Scene 1: This is a visualization of the number of anime shows produced over the years.",
+        "Scene 2: This scene breaks down the number of anime shows each year according to genre.",
+        "Scene 3: This scene displays a scatter plot showing the relationship between user ratings and the number of users who watched the shows."
+    ];
+    
+    // Update the scene description
+    d3.select("#sceneDescription").text(sceneDescriptions[currentSceneIndex]);
+
+    // Generate the current scene
+    scenes[currentSceneIndex]();
+
+    // Enable or disable the Previous button
+    d3.select("#prevButton")
+        .property("disabled", currentSceneIndex === 0);
+
+    // Enable or disable the Next button
+    d3.select("#nextButton")
+        .property("disabled", currentSceneIndex === scenes.length - 1);
+
+    // Hide or display the legend depending on the current scene
+    if (currentSceneIndex === 1) { // If it's Scene 2 (0-indexed)
+        d3.select("#legend").style("display", "block");
+    } else {
+        d3.select("#legend").style("display", "none");
+    }
+}
+
+// Initial scene generation
+generateScene();
+
+// Set up the event listener for the Previous button
+d3.select("#prevButton")
+    .on("click", function() {
+        currentSceneIndex--;
+        generateScene();
+    });
+
+// Set up the event listener for the Next button
+d3.select("#nextButton")
+    .on("click", function() {
+        currentSceneIndex++;
+        generateScene();
+    });
--- scene3.js
+++ scene3.js
@@ -1,85 +1,86 @@
+export default function s3() {
 // Set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 30, left: 60},
-    width = 1000 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    var margin = {top: 10, right: 30, bottom: 30, left: 60},
+        width = 1000 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
 
-// Append the svg object to the body of the page
-var svg = d3.select("#my_dataviz")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right + 150)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+    // Append the svg object to the body of the page
+    var svg = d3.select("#my_dataviz")
+    .append("svg")
+        .attr("width", width + margin.left + margin.right + 150)
+        .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
 
-// Load the data
-d3.json("data/anime_filtered.csv.json").then(function(animeData) {
-    // Pre-process the data: convert 'score', 'members', and 'episodes' to numbers,
-    // and extract the year from the 'aired' field
-    animeData.forEach(function(d) {
-        d.score = +d.score;
-        d.members = +d.members;
-        d.episodes = +d.episodes;
-        d.aired_from_year = new Date(d.aired.from).getFullYear();
-    });
-
-    // Create a color scale for the years
-    var color = d3.scaleSequential()
-        .domain(d3.extent(animeData, function(d) { return d.aired_from_year; }))
-        .interpolator(d3.interpolateBlues);
-
-    // Create a size scale for the number of episodes
-    var size = d3.scaleLinear()
-        .domain(d3.extent(animeData, function(d) { return d.episodes; }))
-        .range([4, 20]);
-
-    // Add X axis
-    var x = d3.scaleLinear()
-        .domain(d3.extent(animeData, function(d) { return d.score; }))
-        .range([0, width]);
-    svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
-
-    // Add Y axis
-    var y = d3.scaleLinear()
-        .domain([0, d3.max(animeData, function(d) { return d.members; })])
-        .range([height, 0]);
-    svg.append("g")
-        .call(d3.axisLeft(y));
-
-    // Add a tooltip div (initially hidden)
-    var tooltip = d3.select("#my_dataviz")
-        .append("div")
-        .style("opacity", 0)
-        .attr("class", "tooltip")
-        .style("background-color", "white")
-        .style("border", "solid")
-        .style("border-width", "2px")
-        .style("border-radius", "5px")
-        .style("padding", "5px");
-
-    // Show the dots
-    svg.append('g')
-        .selectAll("dot")
-        .data(animeData)
-        .enter()
-        .append("circle")
-        .attr("cx", function(d) { return x(d.score); })
-        .attr("cy", function(d) { return y(d.members); })
-        .attr("r", function(d) { return size(d.episodes); })
-        .style("fill", function(d) { return color(d.aired_from_year); })
-        .style("opacity", 0.5)
-        .on("mouseover", function(event, d) {
-            tooltip.style("opacity", 1)
-                .html("Title: " + d.title + "<br>Score: " + d.score + "<br>Members: " + d.members)
-                .style("left", (d3.pointer(event)[0]+30) + "px")
-                .style("top", (d3.pointer(event)[1]+30) + "px");
-        })
-        .on("mouseout", function(d) {
-            tooltip.style("opacity", 0);
+    // Load the data
+    d3.json("data/anime_filtered.csv.json").then(function(animeData) {
+        // Pre-process the data: convert 'score', 'members', and 'episodes' to numbers,
+        // and extract the year from the 'aired' field
+        animeData.forEach(function(d) {
+            d.score = +d.score;
+            d.members = +d.members;
+            d.episodes = +d.episodes;
+            d.aired_from_year = new Date(d.aired.from).getFullYear();
         });
-}).catch(function(error){
-        console.log(error);
-});
-    
+
+        // Create a color scale for the years
+        var color = d3.scaleSequential()
+            .domain(d3.extent(animeData, function(d) { return d.aired_from_year; }))
+            .interpolator(d3.interpolateBlues);
+
+        // Create a size scale for the number of episodes
+        var size = d3.scaleLinear()
+            .domain(d3.extent(animeData, function(d) { return d.episodes; }))
+            .range([4, 20]);
+
+        // Add X axis
+        var x = d3.scaleLinear()
+            .domain(d3.extent(animeData, function(d) { return d.score; }))
+            .range([0, width]);
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x));
+
+        // Add Y axis
+        var y = d3.scaleLinear()
+            .domain([0, d3.max(animeData, function(d) { return d.members; })])
+            .range([height, 0]);
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+        // Add a tooltip div (initially hidden)
+        var tooltip = d3.select("#my_dataviz")
+            .append("div")
+            .style("opacity", 0)
+            .attr("class", "tooltip")
+            .style("background-color", "white")
+            .style("border", "solid")
+            .style("border-width", "2px")
+            .style("border-radius", "5px")
+            .style("padding", "5px");
+
+        // Show the dots
+        svg.append('g')
+            .selectAll("dot")
+            .data(animeData)
+            .enter()
+            .append("circle")
+            .attr("cx", function(d) { return x(d.score); })
+            .attr("cy", function(d) { return y(d.members); })
+            .attr("r", function(d) { return size(d.episodes); })
+            .style("fill", function(d) { return color(d.aired_from_year); })
+            .style("opacity", 0.5)
+            .on("mouseover", function(event, d) {
+                tooltip.style("opacity", 1)
+                    .html("Title: " + d.title + "<br>Score: " + d.score + "<br>Members: " + d.members)
+                    .style("left", (d3.pointer(event)[0]+30) + "px")
+                    .style("top", (d3.pointer(event)[1]+30) + "px");
+            })
+            .on("mouseout", function(d) {
+                tooltip.style("opacity", 0);
+            });
+    }).catch(function(error){
+            console.log(error);
+    });
+}
